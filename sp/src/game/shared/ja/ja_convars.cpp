8ab3439b3077c9710bff480bcec8a4a9d13b277f
Jeremy Agostino <jeremyagost@gmail.com>
2015-09-08 14:59:21 -0400

    Reduced viper damage

--- sp/src/game/shared/ja/ja_convars.cpp
+++ sp/src/game/shared/ja/ja_convars.cpp
@@ -12,5 +12,5 @@
 #include "tier0/memdbgon.h"
 
 ConVar	sk_plr_dmg_avenger		( "sk_plr_dmg_avenger","4", FCVAR_REPLICATED );
-ConVar	sk_plr_dmg_viper		( "sk_plr_dmg_viper","80", FCVAR_REPLICATED );
+ConVar	sk_plr_dmg_viper		( "sk_plr_dmg_viper","60", FCVAR_REPLICATED );
 ConVar	sk_max_heatsink			( "sk_max_heatsink", "9", FCVAR_REPLICATED );
